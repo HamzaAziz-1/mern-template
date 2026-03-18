3c74f703e4f8581a613b3a5f7586786d3fe09868
Muhammad Hamza Aziz <88493437+HamzaAziz-1@users.noreply.github.com>
2026-03-18 21:41:53 +0500

    Test (#1)
    * Add empty line for better readability in main.jsx
    
    * Add color-scheme and background color to CSS

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           {/* <AppProvider> */}
             <App />
           {/* </AppProvider> */}
+          
         </PersistGate>
       </Provider>
     </BrowserRouter>
